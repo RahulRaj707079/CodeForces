236520668383b417dddc479346e133e67a334223
Rahul Kumar <kmarrahul7070@gmail.com>
2025-02-22 21:46:33 +0530

    Watermelon problem

---
+++ 4A_watermelon.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main(){
+     int w;
+     cin>>w;
+     if(w&1 || w<3) cout<<"NO\n";
+     else cout<<"YES\n";
+    return 0;
+}
